4355ff34f17b99089caf774610d6a645beeae070
Lingmo <lingmo@lingmo.org>
2026-07-10 21:09:29 +0800

    fix: use hand-written export.h, remove generate_export_header

---
+++ include/lingmo-appearance/export.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#include <QtCore/qglobal.h>
+
+#if defined(LINGMO_APPEARANCE_LIBRARY)
+#  define LINGMO_APPEARANCE_EXPORT Q_DECL_EXPORT
+#else
+#  define LINGMO_APPEARANCE_EXPORT Q_DECL_IMPORT
+#endif
